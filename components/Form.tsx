6d0e68ad131ad1f04500b5e894dba00c72b53f42
Maricaya <915270549@qq.com>
2020-07-02 20:39:17 +0800

    创建 posts new 页面

--- components/Form.tsx
+++ components/Form.tsx
@@ -6,9 +6,9 @@ type Props = {
     onSubmit: FormEventHandler;
     fields: {
         label: string,
-        type: 'text' | 'password',
+        type: 'text' | 'password' | 'textarea',
         value: string | number,
-        onChange: ChangeEventHandler<HTMLInputElement>,
+        onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
         errors: string[]
     } [];
     buttons: ReactChild
@@ -20,8 +20,11 @@ export const Form: React.FC<Props> = (props) => {
             {props.fields.map(field =>
                 <div>
                     <label>{field.label}
-                        <input type={field.type} value={field.value}
-                            onChange={field.onChange}/>
+                        {field.type === 'textarea'
+                            ? <textarea onChange={field.onChange}>{field.value}</textarea>
+                            : <input type={field.type} value={field.value}
+                                      onChange={field.onChange}/>}
+
                     </label>
                     {field.errors?.length > 0 && <div>
                         {field.errors.join(',')}
--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,10 +1,45 @@
 import {NextPage} from 'next';
 import * as React from 'react';
+import {Form} from '../../components/Form';
+import {useCallback} from 'react';
+import {useState} from 'react';
+import axios, {AxiosResponse} from 'axios';
 
 const PostsNew: NextPage = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        content: ''
+    });
+    const onChange = useCallback((key, value) => {
+        setFormData({...formData, [key]: value})
+    }, [formData]);
+    const [errors, setErrors] = useState({
+        username: [], password: [], passwordConfirmation: []
+    });
+    const onSubmit = useCallback((e) => {
+        e.preventDefault();
+        axios.post(`/api/v1/posts`, formData)
+            .then(() => {
+                window.alert('提交成功');
+            }, (error) => {
+                if (error.response) {
+                    const response: AxiosResponse = error.response;
+                    if (response.status === 422) {
+                        setErrors(response.data);
+                    }
+                }
+            });
+    }, [formData]);
     return (
         <div>
-            PostsNew
+            <Form fields={[
+                {label: '标题', type: 'text', value: formData.title,
+                    onChange: e => onChange('username', e.target.value), errors: errors.username},
+                {label: '内容', type: 'text', value: formData.title,
+                    onChange: e => onChange('username', e.target.value), errors: errors.username}
+            ]} onSubmit={onSubmit} buttons={
+                <button type="submit">提交</button>
+            }/>
         </div>)
 };
 
