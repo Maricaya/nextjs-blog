23f379d7bc9a586499d9b73d13f891bb7aaf324f
Maricaya <915270549@qq.com>
2020-07-17 18:29:24 +0800

    解决 toString

--- lib/post.tsx
+++ lib/post.tsx
@@ -1,36 +1,36 @@
-import path from "path";
-import fs, {promises as fsPromise} from "fs";
-import matter from "gray-matter";
+import path from 'path';
+import fs, {promises as fsPromise} from 'fs';
+import matter from 'gray-matter';
 import marked from 'marked';
 
 
 const markdownDir = path.join(process.cwd(), 'markdown');
 export const getPosts = async () => {
-    const fileNames =  await fsPromise.readdir(markdownDir);
-    const posts = fileNames.map(fileName => {
-        const fullPath = path.join(markdownDir, fileName);
-        const id = fileName.replace(/\.md$/g, '');
-        const text = fs.readFileSync(fullPath, 'utf8');
-        const {data: {title, date}} = matter(text);
-        return {
-          id, title, date
-        }
-    });
-    return posts;
+  const fileNames = await fsPromise.readdir(markdownDir);
+  const posts = fileNames.map(fileName => {
+    const fullPath = path.join(markdownDir, fileName);
+    const id = fileName.replace(/\.md$/g, '');
+    const text = fs.readFileSync(fullPath, 'utf8');
+    const {data: {title, date}} = matter(text);
+    return {
+      id, title, date
+    };
+  });
+  return posts;
 };
 
 export const getPost = async (id: string) => {
-    const fullPath = path.join(markdownDir, `${id}.md`);
-    const text = fs.readFileSync(fullPath, 'utf8');
-    const {data: {title, date}, content} = matter(text);
-    const htmlContent = marked(content)
+  const fullPath = path.join(markdownDir, `${id}.md`);
+  const text = fs.readFileSync(fullPath, 'utf8');
+  const {data: {title, date}, content} = matter(text);
+  const htmlContent = marked(content);
 
-    return {
-        id, title, date, content, htmlContent
-    }
-}
+  return {
+    id, title, date, content, htmlContent
+  };
+};
 
 export const getPostIds = async () => {
-    const fileNames =  await fsPromise.readdir(markdownDir);
-    return fileNames.map(item => item.replace(/\.md$/g, ''));
-}
+  const fileNames = await fsPromise.readdir(markdownDir);
+  return fileNames.map(item => item.replace(/\.md$/g, ''));
+};
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -8,60 +8,61 @@ import qs from 'querystring';
 import {usePager} from '../../hooks/usePager';
 
 type Props = {
-    posts: Post[],
-    count: number,
-    perPage: number,
-    page: number,
-    totalPage: number
+  posts: Post[],
+  count: number,
+  perPage: number,
+  page: number,
+  totalPage: number
 }
 
 const PostsIndex: NextPage<Props> = (props) => {
-    const {posts, count, page, totalPage} = props;
-    const {pager} = usePager({
-        page, totalPage
-    });
-    return (
-        <div>
-            <h1>文章列表({props.count}) 每页{props.perPage}</h1>
-            {posts.map(post =>
-                <div key={post.id}>
-                    <Link href={`/posts/${post.id}`}>
-                        <a>{post.title}</a>
-                    </Link>
-                </div>
-            )}
-            <footer>
-                {pager}
-            </footer>
+  const {posts, count, page, totalPage} = props;
+  const {pager} = usePager({
+    page, totalPage
+  });
+  return (
+    <div>
+      <h1>文章列表({props.count}) 每页{props.perPage}</h1>
+      {posts.map(post =>
+        <div key={post.id}>
+          <Link href={`/posts/${post.id}`}>
+            <a>{post.title}</a>
+          </Link>
         </div>
-    );
+      )}
+      <footer>
+        {pager}
+      </footer>
+    </div>
+  );
 };
 
 export default PostsIndex;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const index = context.req.url.indexOf('?');
-    const search = context.req.url.substr(index + 1);
-    const query = qs.parse(search);
+  const index = context.req.url.indexOf('?');
+  const search = context.req.url.substr(index + 1);
+  const query = qs.parse(search);
 
-    const page = parseInt(query.page.toString()) || 1;
+  const page = parseInt(query.page && query.page.toString()) || 1;
 
-    const connection = await getDatabaseConnection();
-    const perPage = 2;
-    const [posts, count] = await connection.manager.findAndCount(Post, {
-        skip: (page - 1) * perPage, take: perPage}
-    );
-    const ua = context.req.headers['user-agent'];
+  const connection = await getDatabaseConnection();
+  const perPage = 1;
+  const [posts, count] = await connection.manager.findAndCount(Post, {
+      skip: (page - 1) * perPage, take: perPage
+    }
+  );
+  const ua = context.req.headers['user-agent'];
 
-    const result = new UAParser(ua).getResult();
-    return {
-        props: {
-            browser: result.browser,
-            posts: JSON.parse(JSON.stringify(posts)),
-            count,
-            perPage,
-            page,
-            totalPage: Math.ceil(count / perPage)
-        }
-    };
+  const result = new UAParser(ua).getResult();
+  return {
+    props: {
+      browser: result.browser,
+      posts: JSON.parse(JSON.stringify(posts)),
+      count,
+      perPage,
+      page,
+      totalPage: Math.ceil(count / perPage)
+    }
+  };
 };
