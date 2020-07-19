954a41a17ab47eca2f07f3093bc4e762eecd960b
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-07-19 19:30:25 +0800

    Update getDatabaseConnection.tsx

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -9,6 +9,8 @@ const create = async () => {
     // @ts-ignore
     return createConnection({
         ...config,
+        host: process.env.NODE_ENV === 'production' ? 'localhost' : config.host,
+        database: process.env.NODE_ENV === 'production' ? 'blog_production' : 'blog_development',
         entities: [Post, User, Comment]
     });
 };
