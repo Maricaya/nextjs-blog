fb786f83b433230af5c8f6790f981eaab0267b0d
Maricaya <915270549@qq.com>
2020-07-23 13:12:29 +0800

    完善 编辑页面

--- pages/api/v1/posts/[id].tsx
+++ pages/api/v1/posts/[id].tsx
@@ -1,12 +1,13 @@
 import {NextApiHandler} from 'next';
-import {getDatabaseConnection} from '../../../lib/getDatabaseConnection';
 import {Post} from 'src/entity/Post';
-import {withSession} from '../../../lib/withSession';
+import {withSession} from '../../../../lib/withSession';
+import {getDatabaseConnection} from 'lib/getDatabaseConnection';
 
 const Posts: NextApiHandler = withSession(async (req, res) => {
-  if (req.method === 'POST') {
-    const {title, content} = req.body;
-    const post = new Post();
+  if (req.method === 'PATCH') {
+    const {title, content, id} = req.body;
+    const connection = await getDatabaseConnection();
+    const post = await connection.manager.findOne<Post>('Post', id);
     post.title = title;
     post.content = content;
     const user = req.session.get('currentUser');
@@ -16,7 +17,6 @@ const Posts: NextApiHandler = withSession(async (req, res) => {
       return;
     }
     post.author = user;
-    const connection = await getDatabaseConnection();
     await connection.manager.save(post);
     res.json(post);
   }
